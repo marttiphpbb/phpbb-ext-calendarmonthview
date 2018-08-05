d74f0b55c0833b0412685aee1a4adc2d3bec829e
martti <info@martti.be>
2018-08-05 19:10:07 +0200

    viewing listener

--- event/main_listener.php
+++ event/main_listener.php
@@ -7,39 +7,16 @@
 
 namespace marttiphpbb\calendarmonthview\event;
 
-use phpbb\controller\helper;
-use phpbb\template\template;
-use phpbb\language\language;
 use phpbb\event\data as event;
 use marttiphpbb\calendarmonthview\util\cnst;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class main_listener implements EventSubscriberInterface
 {
-	protected $helper;
-	protected $php_ext;
-	protected $template;
-	protected $language;
-
-	public function __construct(
-		helper $helper,
-		string $php_ext,
-		template $template,
-		language $language
-	)
-	{
-		$this->helper = $helper;
-		$this->php_ext = $php_ext;
-		$this->template = $template;
-		$this->language = $language;
-	}
-
 	static public function getSubscribedEvents()
 	{
 		return [
 			'core.user_setup'						=> 'core_user_setup',
-			'core.page_header'						=> 'core_page_header',
-			'core.viewonline_overwrite_location'	=> 'core_viewonline_overwrite_location',
 		];
 	}
 
@@ -52,22 +29,4 @@ class main_listener implements EventSubscriberInterface
 		];
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
-
-	public function core_page_header(event $event)
-	{
-/*
-		$this->template->assign_vars([
-			'U_MARTTIPHPBB_CALENDARMONTHVIEW'			=> $this->helper->route('marttiphpbb_calendarmonthview_defaultview_controller'),
-		]); */
-	}
-
-	public function core_viewonline_overwrite_location(event $event)
-	{
-		if (strrpos($event['row']['session_page'], 'app.' . $this->php_ext . '/calendarmonthview') === 0)
-		{
-			$event['location'] = $this->language->lang(cnst::L . '_VIEWING');
-			$event['location_url'] = $this->helper->route('marttiphpbb_calendarmonthview_defaultview_controller');
-		}
-	}
-
 }
