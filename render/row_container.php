ec2066c26985a05801f3041d4cd2ab0ec2e78686
martti <info@martti.be>
2018-09-01 12:41:47 +0200

    overflow hidden

--- render/row_container.php
+++ render/row_container.php
@@ -15,19 +15,19 @@ use marttiphpbb\calendarmonthview\render\calendar_event_row;
 class row_container
 {
 	protected $rows = [];
-	protected $max_rows = 50;
+	protected $max_rows;
 
 	public function __construct(int $min_rows, int $max_rows)
 	{
 		for ($row_index = 0; $row_index < $min_rows; $row_index++)
 		{
-			$this->get_or_create_row($row_index);
+			$this->get_row($row_index);
 		}
 
 		$this->max_rows = $max_rows;
 	}
 
-	private function get_or_create_row(int $row_index):calendar_event_row
+	private function get_row(int $row_index):calendar_event_row
 	{
 		if (!$this->rows[$row_index])
 		{
@@ -41,7 +41,7 @@ class row_container
 	{
 		for($row_index = 0; $row_index < $this->max_rows; $row_index++)
 		{
-			$row = $this->get_or_create_row($row_index);
+			$row = $this->get_row($row_index);
 
 			if (!is_null($free_segment_index = $row->get_free_segment_index($calendar_event)))
 			{
